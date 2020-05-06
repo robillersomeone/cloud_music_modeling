c438e80853599053cdb561f97b641a65bfabcb59
Robert Hillery <robert.hillery@gmail.com>
2020-05-06 09:16:01 -0400

    update db schema

--- create_tables.py
+++ create_tables.py
@@ -3,16 +3,32 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
+    """
+    instance of the cursor, connection to database,
+    function to drop all tables if they exist
+    commits changes to tables in redshift
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 def create_tables(cur, conn):
+    """
+    instance of the cursor, connection to database,
+    function to create staging and star tables
+    commits changes to tables in redshift
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 def main():
+    """
+    main function
+    read config, connect to redshift cluster
+    calls drop tables and create tables
+    closes the connection to the database 
+    """
     config = configparser.ConfigParser()
     config.read_file(open('dwh.cfg'))
 
--- etl.py
+++ etl.py
@@ -3,16 +3,35 @@ import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 def load_staging_tables(cur, conn):
+    """
+    instance of the cursor, connection to database,
+    function to load staging tables
+    COPY events s3 bucket
+    COPY song metadata s3 bucket
+    commits changes to tables in redshift
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 def insert_tables(cur, conn):
+    """
+    instance of the cursor, connection to database,
+    function to load star tables
+    commits changes to tables in redshift
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 def main():
+    """
+    main function
+    read config, connect to redshift cluster
+    COPY data from s3 to staging data
+    insert data into star tables
+    closes the connection to the database
+    """
     config = configparser.ConfigParser()
     config.read_file(open('dwh.cfg'))
 
