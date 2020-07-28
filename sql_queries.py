0bb9cea3aa41626c026f0f8e5c7ce9c575e9c39a
robert hillery <39356742+robillersomeone@users.noreply.github.com>
2020-07-27 21:57:29 -0500

    updates songs query

--- sql_queries.py
+++ sql_queries.py
@@ -211,19 +211,17 @@ ON a.artist_id = s.artist_id
 LIMIT 10;
 """)
 
-pop_genre = ("""
-""")
-
-pop_song = ("""
-SELECT s.title
+songs = ("""
+SELECT COUNT(DISTINCT(s.title))
 FROM songs s
 JOIN songplays sp
 ON s.song_id = sp.song_id
 """)
+
 
 # queries
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-analytics_table_queries = [pop_artists, pop_genre, pop_song]
+analytics_table_queries = [pop_artists, songs]
